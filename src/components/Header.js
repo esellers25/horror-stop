ab278fd185daf698ffcb69b1ebf5a0dca4012ac7
esellers25 <erin.e.sellers@gmail.com>
2021-05-31 23:04:06 -0400

    logout button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,13 @@
 function Header() {
+    
+    function logOut(){
+        localStorage.clear()
+    }
+    
     return (
         <div>
             <h1>SPOOKY SITE</h1>
+            <button onClick={() => logOut()}>LogOut</button>
         </div>
     )
 }
